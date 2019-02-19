7d5bdc0e99e41a64ad4842ec130af4f4ae272f56
Michele Finotto <m@finotto.org>
2019-02-19 11:25:12 +0000

    [NEW] Initial import

---
+++ japierr.go
@@ -0,0 +1,52 @@
+import "fmt"
+
+type Error struct {
+	Status string `json:"status"`
+	Source Source `json:"source"`
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
+
+type Source interface{}
+
+type Pointer struct {
+	Pointer string `json:"pointer"`
+}
+
+type Parameter struct {
+	Parameter string `json:"parameter"`
+}
+
+type Errors struct {
+	Errors []*Error `json:"errors"`
+}
+
+func NewErrors() Errors {
+	return Errors{
+		Errors: []*Error{},
+	}
+}
+
+func (errs *Errors) Append(e *Error) {
+	errs.Errors = append(err.Errors, e)
+}
+
+func (errs Errors) IsEmpty() bool {
+	return len(errs.Errors) == 0
+}
+
+func (errs Errors) HasErrors() bool {
+	return len(errs.Errors) > 0
+}
+
+func newInvalidAttribute(attr, detail string) *Error {
+	return &Error{
+		Status: "422",
+		Source: Pointer{
+			Pointer: fmt.Sprintf("data/attributes/%s", attr),
+		},
+		Title:  "Invalid Attribute",
+		Detail: detail,
+	}
+}
+
